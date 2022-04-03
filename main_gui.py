f6ade5a6a821698f79c8836f8a6346a26f0d3773
tony.lo <tony.lo@arch-partnership.com>
2022-04-03 17:50:39 +0800

    update: main_gui folder format fix

--- main_gui.py
+++ main_gui.py
@@ -103,55 +103,30 @@ class MainGUI:
             print("User click sth !")
 
             #Learning module 1 onLick handling condition
-            if targetX >= self.lmCrood1["x"] - lmGifSize[
-                    "width"] / 2 and targetX <= self.lmCrood1["x"] + lmGifSize[
-                        "width"] / 2 and targetY >= self.lmCrood1[
-                            "y"] - lmGifSize[
-                                "height"] / 2 and targetY <= self.lmCrood1[
-                                    "y"] + lmGifSize["height"] / 2:
+            if targetX >= self.lmCrood1["x"] - lmGifSize["width"] / 2 and targetX <= self.lmCrood1["x"] + lmGifSize["width"] / 2 and targetY >= self.lmCrood1["y"] - lmGifSize["height"] / 2 and targetY <= self.lmCrood1["y"] + lmGifSize["height"] / 2:
                 print("We click learning mocdule 1 !!")
                 learn_module1 = lm1_gui.lm1GUI()
                 learn_module1.run()
 
             #Learning module 2 onLick handling condition
-            if targetX >= self.lmCrood2["x"] - lmGifSize[
-                    "width"] / 2 and targetX <= self.lmCrood2["x"] + lmGifSize[
-                        "width"] / 2 and targetY >= self.lmCrood2[
-                            "y"] - lmGifSize[
-                                "height"] / 2 and targetY <= self.lmCrood2[
-                                    "y"] + lmGifSize["height"] / 2:
+            if targetX >= self.lmCrood2["x"] - lmGifSize["width"] / 2 and targetX <= self.lmCrood2["x"] + lmGifSize["width"] / 2 and targetY >= self.lmCrood2["y"] - lmGifSize["height"] / 2 and targetY <= self.lmCrood2["y"] + lmGifSize["height"] / 2:
                 print("We click learning mocdule 2 !!")
                 learn_module2 = lm2_gui.lm2GUI()
                 learn_module2.run()
 
             #Learning module 3 onLick handling condition
-            if targetX >= self.lmCrood3["x"] - lmGifSize[
-                    "width"] / 2 and targetX <= self.lmCrood3["x"] + lmGifSize[
-                        "width"] / 2 and targetY >= self.lmCrood3[
-                            "y"] - lmGifSize[
-                                "height"] / 2 and targetY <= self.lmCrood3[
-                                    "y"] + lmGifSize["height"] / 2:
+            if targetX >= self.lmCrood3["x"] - lmGifSize["width"] / 2 and targetX <= self.lmCrood3["x"] + lmGifSize["width"] / 2 and targetY >= self.lmCrood3["y"] - lmGifSize["height"] / 2 and targetY <= self.lmCrood3["y"] + lmGifSize["height"] / 2:
                 print("We click learning mocdule 3 !!")
                 learn_module3 = lm3_gui.lm3GUI()
                 learn_module3.run()
 
             #Learning module 4 onLick handling condition
-            if targetX >= self.lmCrood4["x"] - lmGifSize[
-                    "width"] / 2 and targetX <= self.lmCrood4["x"] + lmGifSize[
-                        "width"] / 2 and targetY >= self.lmCrood4[
-                            "y"] - lmGifSize[
-                                "height"] / 2 and targetY <= self.lmCrood4[
-                                    "y"] + lmGifSize["height"] / 2:
+            if targetX >= self.lmCrood4["x"] - lmGifSize["width"] / 2 and targetX <= self.lmCrood4["x"] + lmGifSize["width"] / 2 and targetY >= self.lmCrood4["y"] - lmGifSize["height"] / 2 and targetY <= self.lmCrood4["y"] + lmGifSize["height"] / 2:
                 print("We click learning mocdule 4 !!")
                 learn_module4 = lm4_gui.lm4GUI()
                 learn_module4.run()
 
-            if targetX >= self.exitButtonCrood[
-                    "x"] and targetX <= self.exitButtonCrood[
-                        "x"] + self.exitButtonSize[
-                            "width"] and targetY >= self.exitButtonCrood[
-                                "y"] and targetY <= self.exitButtonCrood[
-                                    "y"] + self.exitButtonSize["height"]:
+            if targetX >= self.exitButtonCrood["x"] and targetX <= self.exitButtonCrood["x"] + self.exitButtonSize["width"] and targetY >= self.exitButtonCrood["y"] and targetY <= self.exitButtonCrood["y"] + self.exitButtonSize["height"]:
                 self.closeWin()
                 break
 
