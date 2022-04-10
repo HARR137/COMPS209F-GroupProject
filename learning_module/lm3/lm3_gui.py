e31fb04359ff778f84c606306e10a15cc18e8ad1
tony.lo <tony.lo@arch-partnership.com>
2022-04-11 02:02:24 +0800

    feat: aas module implement

--- learning_module/lm3/lm3_gui.py
+++ learning_module/lm3/lm3_gui.py
@@ -1,39 +1,23 @@
 from library.graphics import *
 from gobal_var import fontSize
+from common import createMsgBox, createImg, createRectangle,createInputBox
 import random
-from common import createMsgBox, createImg, createRectangle, createCircle
 
-
-MSG_1 = "Correct answer. Well done!Keep going!"
-MSG_2 = "Wrong answer. Add oil!"
-MSG_3 = ""
-
-
-
-def checkAnswer(randnum, ans):
-    if (randnum % 2 == 0) and (ans == "Odd"):
-        Correct = False
-    elif (randnum % 2 != 0) and (ans == "Odd"):
-        Correct = True
-    elif (randnum % 2 != 0) and (ans == "Even"):
-        Correct = False
-    elif (randnum % 2 == 0) and (ans == "Even"):
-        Correct = True
-    else:
-        return None # invalid input
-    return Correct
 
 class lm3GUI():
     def __init__(self):
+        # self.expression = ["+", "-", "*", "/"]
+        self.expression = ["+", "-"]
+        self.gameTurns = 5
+        self.questions = []
+        self.score = 0
+        self.enterButtonClicked = False
         pass
-
+        
+        
     def run(self):
         self.winWidth = 500
         self.winHeight = 500
-        self.score = 0  # initialize score to 0
-        self.attempt = 0  # how many questions answers
-        self.max_attempt = 3  # how many questions total
-
 
         #ExitButton
         self.exitButtonSize= { "width": 50, "height": 25 }
@@ -42,10 +26,12 @@ class lm3GUI():
         self.win = GraphWin('Learning Module 3', self.winWidth, self.winHeight)
         self.exitButton = createRectangle(
             self,
-            x = self.exitButtonCrood["x"],
-            y = self.exitButtonCrood["y"],
-            width = self.exitButtonSize["width"],
-            height = self.exitButtonSize["height"]
+            starting_x = self.exitButtonCrood["x"],
+            starting_y = self.exitButtonCrood["y"],
+            ending_x = self.exitButtonSize["width"],
+            ending_y = self.exitButtonSize["height"], 
+            fillColor = None, 
+            outlineColor = None
         )
         self.exitButtonText = createMsgBox(
             self,
@@ -56,101 +42,172 @@ class lm3GUI():
             color = "black",
             fontSize = fontSize["sFont"]
         )
-
-        MsgBox = Text(Point(250, 150), "This number is an odd or evne number?(Enter Odd/Even)")
-        MsgBox.draw(self.win)
-
-        StatusBox = Text(Point(250, 240), "")
-        StatusBox.draw(self.win)
-
-
-        while True:
-            if self.attempt >= self.max_attempt:  # finished all attempts
-                self.updateResult('Completed.Your score is {}/{}'.format(self.score, self.attempt))
-                break
-
-
-            QuestionBox = Text(Point(250, 180), random.randint(1, 10))
-            QuestionBox.draw(self.win)
-            randnum = QuestionBox.getText()
-            self.updateStatus("")
-            while True:
-                ans = self.waitGetInput()
-                Correct = checkAnswer(randnum, ans)
-                if Correct == True:
-                     self.updateStatus(MSG_1)
-                     self.score += 1
-                     self.attempt += 1
-                     QuestionBox.setText("")
-                else:
-                     self.updateStatus(MSG_2)
-                     self.attempt += 1
-                     QuestionBox.setText("")
-                break
-                  
-
-        self.exitButtonSize = { "width": 50, "height": 25 }
-        self.exitButtonCrood = { "x": 5, "y": 50 }
-        
-        self.win = GraphWin('Learning Module 4', self.winWidth, self.winHeight)
-
-        self.exitButtonSize = {"radius": 25}
-        self.exitButtonCrood = {"x": 5, "y": 5}
-        self.exitButton = createCircle(self,
-                                          x=self.exitButtonCrood["x"],
-                                          y=self.exitButtonCrood["y"],
-                                          radius=self.exitButtonSize["radius"], 
-                                          fillColor = "#C73618", 
-                                          outlineColor = "#C73618")
-        self.exitButtonText = createMsgBox(
+        self.enterButtonCrood = { "starting_x": 115, "starting_y": 380, "ending_x": 215, "ending_y": 410 }
+        self.enterTextCrood = { "x": 165, "y": 395 }
+        self.enterButton = createRectangle(
             self,
-            msg="x",
-            x=self.exitButtonCrood["x"] + 8.4, 
-            y=self.exitButtonCrood["y"] + 7.4, 
-            color="white",
-            fontSize=fontSize["mFont"])
+            starting_x = self.enterButtonCrood["starting_x"],
+            starting_y = self.enterButtonCrood["starting_y"], 
+            ending_x = self.enterButtonCrood["ending_x"],
+            ending_y = self.enterButtonCrood["ending_y"],
+            fillColor = "#159947", 
+            outlineColor = "#159947"
+        )
+        self.enterButtonText = createMsgBox(
+            self,
+            msg = "ENTER",
+            x = self.enterTextCrood["x"],
+            y = self.enterTextCrood["y"],
+            color = "white",
+            fontSize = fontSize["sFont"]
+        )
 
+
+        self.nextButtonCrood = { "starting_x": 285, "starting_y": 380, "ending_x": 385, "ending_y": 410 }
+        self.nextTextCrood = { "x": 335, "y": 395 }
+        self.nextButton = createRectangle(
+            self,
+            starting_x = self.nextButtonCrood["starting_x"],
+            starting_y = self.nextButtonCrood["starting_y"], 
+            ending_x = self.nextButtonCrood["ending_x"],
+            ending_y = self.nextButtonCrood["ending_y"],
+            fillColor = "#0193A5", 
+            outlineColor = "#0193A5"
+        )
+        self.nextButtonText = createMsgBox(
+            self,
+            msg = "NEXT QUESTION",
+            x = self.nextTextCrood["x"],
+            y = self.nextTextCrood["y"],
+            color = "white",
+            fontSize = fontSize["sFont"]
+        )
+
+        self.inputBox = createInputBox(self, x=250, y=300, length=20)
+
+        
+        self.generateQuestions()
         self.lm1OnClickHandler(self.win)
-        return 1
-
+        return self.score
 
     def closeWin(self):
         self.win.close()
 
-    def updateStatus(self, text):
-        StatusBox = Text(Point(250, 240), "")
-        StatusBox.draw(self.win)
-        StatusBox.setText(text)
+    def checkAnswer(self, queston):
+        userInput = int( self.inputBox.getText())
 
-    def updateResult(self, text):
-        resultBox = Text(Point(250, 270), "")
-        resultBox.draw(self.win)
-        resultBox.setText(text)
+        if userInput == eval(queston):
+            self.score = self.score+1 
+            return True
+        else:
+            return False
+
+    def mergeTwoList(self, list1, list2, position):
+        for i, element in enumerate(list2, 1):
+            list1.insert(i * position - 1, element)
+        return list1
+
+    def generateQuestions(self):
+        
+        lostOfNums = []
+        temp = []
+        listOfSymbol = []
+        tempSymbol = []
+
+        for turns in range(self.gameTurns):
+            howManynumberInQuestion = ( random.randint(2,4) )
+            # print("howManynumberInQuestion : ", howManynumberInQuestion)
+            for index, number in enumerate(range(howManynumberInQuestion)):
+                temp.append(random.randint(1,9))
+                if index != howManynumberInQuestion-1:
+                    tempSymbol.append(self.expression[random.randint(0, 1)] )
+            lostOfNums.append(temp)
+            temp = []
+            listOfSymbol.append(tempSymbol)
+            tempSymbol = []
+
+        print(lostOfNums)
+        print(listOfSymbol)
+
+        tempQuestions = []
+        for index, item in enumerate(lostOfNums):
+            temp = self.mergeTwoList(lostOfNums[index], listOfSymbol[index] ,2)
+            tempQuestions.append(temp)
+            
+
+        for index, question in enumerate(tempQuestions):
+            self.questions.append("".join(map(str, question)))
 
 
-    def waitGetInput(self):
-        inputbox = Entry(Point(250, 210), 10)
-        inputbox.draw(self.win)
-        self.win.getMouse()
-        ans = inputbox.getText()
-        try: 
-            return ans
-        except ValueError:
-            pass
-        return None
+    def drawQuestions(self, index):
+        print(self.questions)
+        self.currentTitle =  createMsgBox(
+                                    self,
+                                    msg="Question" + str(index+1),
+                                    x=250 ,
+                                    y=100,
+                                    color="black",
+                                    fontSize=fontSize["xxlFont"]
+                                )
+
+        self.currentQuestion =  createMsgBox(
+                                    self,
+                                    msg=self.questions[index],
+                                    x=250 ,
+                                    y=150,
+                                    color="black",
+                                    fontSize=fontSize["xxlFont"]
+                                )
+            
 
     def lm1OnClickHandler(self, win):
+        counter = 0
+        finishAnswerQuestion = True
         while True:
+            
+            if finishAnswerQuestion:
+                self.drawQuestions(counter)
+                finishAnswerQuestion = False
+                print("finishAnswerQuestion", finishAnswerQuestion)
             checkMouse = win.getMouse()
             targetX = checkMouse.getX()
             targetY = checkMouse.getY()
             
-            if targetX >= self.exitButtonCrood["x"] - self.exitButtonSize["radius"] and targetX <= self.exitButtonCrood["x"] + self.exitButtonSize["radius"] and targetY >= self.exitButtonCrood["y"] - self.exitButtonSize["radius"] and targetY <= self.exitButtonCrood["y"] + self.exitButtonSize["radius"]:
+
+            resultMessage = lambda ans: "Very good! It's correct!" if (ans) else "Wrong answer... Practice makes perfect!"
+            resultMessageColor = lambda ans: "green" if (ans) else "red"
+
+            #enterButton onClick
+            if targetX >= self.enterButtonCrood["starting_x"] and targetX <= self.enterButtonCrood["ending_x"] and targetY >= self.enterButtonCrood["starting_y"] and targetY <= self.enterButtonCrood["ending_y"]:
+                self.enterButtonClicked = True
+                checkedAnswer = self.checkAnswer(self.questions[counter])
+                self.correctOrNot = createMsgBox(
+                            self,
+                            msg=resultMessage(checkedAnswer),
+                            x=250 ,
+                            y=250,
+                            color=resultMessageColor(checkedAnswer),
+                            fontSize=fontSize["lFont"]
+                            )
+                print(checkedAnswer)
+
+            # nextButton onClick
+            if self.enterButtonClicked == True and targetX >= self.nextButtonCrood["starting_x"] and targetX <= self.nextButtonCrood["ending_x"] and targetY >= self.nextButtonCrood["starting_y"] and targetY <= self.nextButtonCrood["ending_y"]:
+                finishAnswerQuestion = True
+                self.currentTitle.undraw()
+                self.currentQuestion.undraw()
+                self.correctOrNot.undraw()
+                counter += 1
+                if counter == 4:
+                    self.closeWin()
+                    break
+      
+            #exitButton onClick
+            if targetX >= self.exitButtonCrood["x"]  and targetX <= self.exitButtonCrood["x"] + self.exitButtonSize["width"] and targetY >= self.exitButtonCrood["y"] and targetY <= self.exitButtonCrood["y"] + self.exitButtonSize["height"] :
                 self.closeWin()
                 break
-
                     
 
 if __name__ == "__main__":
-    lm3_GUI = lm3GUI()
+    lm3GUI = lm3GUI()
     lm3GUI.run()
